95ff12f6f5bdb88462bcfde5ec6908423af152a5
Rathansr13 <rathansracharya466@gmail.com>
2024-03-31 20:16:58 +0530

    second commit

--- src/components/pages/Results.jsx
+++ src/components/pages/Results.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
-
+import '../css/results.css'
+import logo from '../assets/logo.png'
+import strokeImg from '../assets/13343.png'
+import { Link } from 'react-router-dom'
 const Results = () => {
   return (
-    <div>Results</div>
+    <div>
+      <div className='container-img'>
+      <Link to='/'><button className='btn'>back</button></Link>
+        <h1>Result Analysis</h1>
+        <div className='container-result'>
+         <img src={strokeImg} className='img-container-result'/>
+        </div>
+        <div className='container'>
+            <div className='img-container'>
+            <img src={strokeImg} className='img-container-result-card'/>
+            <caption className='font-semibold'>RAW CT SCAN</caption>
+            </div>
+
+            <div className='img-container'>
+            <img src={strokeImg} className='img-container-result-card'/>
+            <caption  className='font-semibold '>MASKED IMAGE</caption>
+            </div>
+
+            <div className='img-container'>
+            <img src={strokeImg} className='img-container-result-card'/>
+            <caption  className='font-semibold'>AFFECTED AREA</caption>
+            </div>
+
+            <div className='img-container'>
+            <img src={strokeImg} className='img-container-result-card'/>
+            <caption  className='font-semibold'>ORIGINAL</caption>
+            </div>
+            
+        </div>
+       
+      </div>
+    
+    </div>
   )
 }
 
