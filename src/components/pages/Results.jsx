691af245de93f76b786391da8a5a8f67ebcca994
Rathansr13 <rathansracharya466@gmail.com>
2024-04-01 21:38:55 +0530

    added images and changed the results page

--- src/components/pages/Results.jsx
+++ src/components/pages/Results.jsx
@@ -3,6 +3,11 @@ import '../css/results.css'
 import logo from '../assets/logo.png'
 import strokeImg from '../assets/13343.png'
 import { Link } from 'react-router-dom'
+import ctScan from '../assets/strokeSamples/10657.png'
+import maskImage from '../assets/strokeSamples/10657_mask.png'
+import bgImage from '../assets/strokeSamples/10657_segmented_bg.png'
+import fgImage from '../assets/strokeSamples/10657_segmented_fg.png'
+import highlighted from '../assets/strokeSamples/10657_highlighted.png'
 const Results = () => {
   return (
     <div>
@@ -10,28 +15,28 @@ const Results = () => {
       <Link to='/'><button className='btn'>back</button></Link>
         <h1>Result Analysis</h1>
         <div className='container-result'>
-         <img src={strokeImg} className='img-container-result'/>
+         <img src={ctScan} className='img-container-result'/>
         </div>
         <div className='container'>
             <div className='img-container'>
-            <img src={strokeImg} className='img-container-result-card'/>
-            <caption className='font-semibold'>RAW CT SCAN</caption>
+            <img src={highlighted} className='img-container-result-card'/>
+            <caption className='font-semibold'>Stroke effected region</caption>
             </div>
 
             <div className='img-container'>
-            <img src={strokeImg} className='img-container-result-card'/>
-            <caption  className='font-semibold '>MASKED IMAGE</caption>
+            <img src={fgImage} className='img-container-result-card'/>
+            <caption  className='font-semibold '>foreground stroke region</caption>
             </div>
 
             <div className='img-container'>
-            <img src={strokeImg} className='img-container-result-card'/>
-            <caption  className='font-semibold'>AFFECTED AREA</caption>
+            <img src={bgImage} className='img-container-result-card'/>
+            <caption  className='font-semibold'>background region</caption>
             </div>
-
             <div className='img-container'>
-            <img src={strokeImg} className='img-container-result-card'/>
-            <caption  className='font-semibold'>ORIGINAL</caption>
+            <img src={maskImage} className='img-container-result-card'/>
+            <caption  className='font-semibold'>Mask region</caption>
             </div>
+          
             
         </div>
        
